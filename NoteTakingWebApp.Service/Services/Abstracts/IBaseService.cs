6353449f93a7508feed8d75d73f9ecf6ec74be1f
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-27 00:25:50 +0400

    Create base service

---
+++ NoteTakingWebApp.Service/Services/Abstracts/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace NoteTakingWebApp.Service.Services.Abstracts
+{
+    public interface IBaseService<TRequestDto, TEntity, TResponseDto>
+    {
+        TResponseDto Create(TRequestDto requestDto);
+        TResponseDto GetById(int id);
+        IEnumerable<TResponseDto> GetAll();
+        TResponseDto Update(TRequestDto requestDto);
+        void Delete(int id);
+    }
+}
---
+++ NoteTakingWebApp.Service/Services/Implementations/BaseService.cs
@@ -0,0 +1,33 @@
+﻿using NoteTakingWebApp.Service.Services.Abstracts;
+
+namespace NoteTakingWebApp.Service.Services.Implementations
+{
+    public class BaseService<TRequestDto, TEntity, TResponseDto> 
+        : IBaseService<TRequestDto, TEntity, TResponseDto>
+    {
+        public virtual TResponseDto Create(TRequestDto requestDto)
+        {
+
+        }
+
+        public virtual TResponseDto GetById(int id)
+        {
+
+        }
+
+        public virtual IEnumerable<TResponseDto> GetAll()
+        {
+
+        }
+
+        public virtual TResponseDto Update(TRequestDto requestDto)
+        {
+
+        }
+
+        public virtual void Delete(int id)
+        {
+
+        }
+    }
+}
