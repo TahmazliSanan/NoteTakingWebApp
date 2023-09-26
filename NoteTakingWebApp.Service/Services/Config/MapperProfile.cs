49594a1e5ac72c1c63cbf2688fbeadc9f7045131
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-27 01:03:14 +0400

    Create MapperProfile

---
+++ NoteTakingWebApp.Service/Services/Config/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NoteTakingWebApp.DataAccess.Entities;
+using NoteTakingWebApp.DTO.Dtos;
+
+namespace NoteTakingWebApp.Service.Services.Config
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Note, NoteDto>().ReverseMap();
+        }
+    }
+}
