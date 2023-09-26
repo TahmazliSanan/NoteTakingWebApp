9559ecf0ec15799ef85ad1340e33f5c782cc5de4
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-27 01:28:55 +0400

    Create NoteService

---
+++ NoteTakingWebApp.Service/Services/Abstracts/INoteService.cs
@@ -0,0 +1,9 @@
+﻿using NoteTakingWebApp.DataAccess.Entities;
+using NoteTakingWebApp.DTO.Dtos;
+
+namespace NoteTakingWebApp.Service.Services.Abstracts
+{
+    public interface INoteService : IBaseService<NoteDto, Note, NoteDto>
+    {
+    }
+}
---
+++ NoteTakingWebApp.Service/Services/Implementations/NoteService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using NoteTakingWebApp.DataAccess.Db;
+using NoteTakingWebApp.DataAccess.Entities;
+using NoteTakingWebApp.DTO.Dtos;
+using NoteTakingWebApp.Service.Services.Abstracts;
+
+namespace NoteTakingWebApp.Service.Services.Implementations
+{
+    public class NoteService : BaseService<NoteDto, Note, NoteDto>, INoteService
+    {
+        public NoteService(IMapper mapper, AppDbContext appDbContext) 
+            : base(mapper, appDbContext)
+        {
+        }
+    }
+}
