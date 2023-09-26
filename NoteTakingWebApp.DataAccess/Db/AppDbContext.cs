42f00e9a947b60ce69a542afe46ff0fb995f4e11
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-26 23:57:08 +0400

    Create AppDbContext

---
+++ NoteTakingWebApp.DataAccess/Db/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteTakingWebApp.DataAccess.Entities;
+
+namespace NoteTakingWebApp.DataAccess.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
