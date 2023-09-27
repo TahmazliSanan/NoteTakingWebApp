37e4b628c1734e5c276b411f4081ca34345ad39b
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-27 13:19:02 +0400

    Create HomeController

---
+++ NoteTakingWebApp.UI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NoteTakingWebApp.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
