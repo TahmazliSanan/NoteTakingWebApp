5657f008d19f03f265edf9ed4884b39ae24b89b1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-09-27 14:39:19 +0400

    Add new properties

--- NoteTakingWebApp.DTO/Dtos/NoteDto.cs
+++ NoteTakingWebApp.DTO/Dtos/NoteDto.cs
@@ -8,5 +8,8 @@ namespace NoteTakingWebApp.DTO.Dtos
         public string Title { get; set; }
         [Required(ErrorMessage = "Content cannot be empty!")]
         public string Content { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public DateTime UpdatedTime { get; set; }
+        public bool IsUpdated { get; set; } = false;
     }
 }
